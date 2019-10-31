3367bc68a98847e07aa4c82ead000b4a0874b1a6
Diego Valera <diego.valera.duran@everis.com>
2019-10-31 09:05:09 +0100

    gitflow-hotfix-stash: fix-exception-handling
    Removes old implementation of Exception (DAOException) and uses the Exceptions and their mappers to provide a accurate error message to the client.
    
    - DataNotFoundException & DataNotFoundExceptionMapper

--- src/main/java/org/diego/tutorial/car/databases/IJPA.java
+++ src/main/java/org/diego/tutorial/car/databases/IJPA.java
@@ -2,14 +2,12 @@ package org.diego.tutorial.car.databases;
 
 import java.util.List;
 
-import org.diego.tutorial.car.exceptions.DAOException;
-
 public interface IJPA {
 	public <T> List<T> getAll(Class<T> type);
 	
-	public <T> T get(Class<T> type, long id) throws DAOException;
+	public <T> T get(Class<T> type, long id);
 	
-	public <T> T add(T entity) throws DAOException;
+	public <T> T add(T entity);
 	
 	public <T> T update(T entity);
 	
