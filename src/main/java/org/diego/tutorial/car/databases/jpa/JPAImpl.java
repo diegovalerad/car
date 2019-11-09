6f824eefcb71705655b6149fba812b11c3a0d960
Diego Valera <diego.valera.duran@everis.com>
2019-11-09 10:17:37 +0100

    Removing not reachable exception
    While trying to add a new object through JPA, it is impossible that that object already exists in database, because it's the database the one that gives ids to each object.

--- src/main/java/org/diego/tutorial/car/databases/jpa/JPAImpl.java
+++ src/main/java/org/diego/tutorial/car/databases/jpa/JPAImpl.java
@@ -5,11 +5,9 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceException;
 import javax.persistence.TypedQuery;
 
 import org.diego.tutorial.car.databases.IJPA;
-import org.diego.tutorial.car.exceptions.DataAlreadyExistsException;
 import org.diego.tutorial.car.exceptions.DataNotFoundException;
 
 @Stateless
@@ -38,11 +36,7 @@ public class JPAImpl implements IJPA {
 
 	@Override
 	public <T> T add(T entity) {
-		try {
-			em.persist(entity);
-		} catch (PersistenceException e) {
-			throw new DataAlreadyExistsException("Trying to add an object that already exists");
-		}
+		em.persist(entity);
 		
 		return entity;
 	}
